eb32f0473e8a97238bbafaa9afee55252394f6cd
John Stoermer <jstoerm@purdue.edu>
2020-01-25 18:40:35 -0500

    button

--- src/components/Farm/index.js
+++ src/components/Farm/index.js
@@ -5,27 +5,41 @@ import Navigation from '../Navigation';
 
 var money = 0;
 
-function updateLabels() {
-  document.getElementById('money').innerHTML = money;
+function updateLabel(label, value) {
+  document.getElementById(label).innerHTML = value;
 }
 
 function makeMoney() {
   money++;
-  updateLabels();
+  updateLabel('money', money);
+}
+
+function updateCrop() {
+
 }
 
 // http://cssgridgarden.com/images/dirt.svg
 
 const Farm = () => (
   <div>
-    <div style={{'display':'flex','justify-content':'center','align-items':'center','height':'100vh'}}>
-      <div>
-        Money: <span id='money'></span>
-        <a class='btn btn-primary' onClick={makeMoney}>Make Money</a>
+    {/*LEFT BAR*/}
+    <div class='row'>
+      <div class='col-md-9' style={{'height': '100vh', 'background-color': 'red'}}>
+        <div style={{'display':'flex','justify-content':'center','align-items':'center','height':'100%'}}>
+          <div class='grid-container'>
+            <div class='grid-item' id='plot-0' onClick={updateCrop}>
+              <img src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class='grid-container'>
-        <div class='grid-item' id='plot-0' onClick={makeMoney}>
-          <img src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+      {/*RIGHT BAR*/}
+      <div class='col-md-3' style={{'height': '100vh', 'background-color': 'green'}}>
+        <div style={{'display':'flex','justify-content':'center','align-items':'center','height':'100%'}}>
+          <div class='row'>
+            <div class='btn btn-primary'>asdf</div>
+            <div class='btn btn-primary'>asdf</div>
+          </div>
         </div>
       </div>
     </div>
