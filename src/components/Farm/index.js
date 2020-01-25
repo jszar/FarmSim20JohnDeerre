9cdab1d82183a82252207a9e38234d6f5ce78a38
John Stoermer <jstoerm@purdue.edu>
2020-01-25 16:33:28 -0500

    fuck

--- src/components/Farm/index.js
+++ src/components/Farm/index.js
@@ -13,12 +13,48 @@ function makeMoney() {
   updateLabels();
 }
 
+// http://cssgridgarden.com/images/dirt.svg
+
 const Farm = () => (
-  <div>
-    Money: <span id='money'></span>
-    <a class='btn btn-primary' onClick={makeMoney}>Make Money</a>
+  <div style={{'display':'flex','justify-content':'center','align-items':'center','height':'100vh'}}>
+    <div class="grid-container">
+      <div class="grid-item">1</div>
+      <div class="grid-item">2</div>
+      <div class="grid-item">3</div>
+      <div class="grid-item">4</div>
+      <div class="grid-item">5</div>
+      <div class="grid-item">6</div>
+      <div class="grid-item">7</div>
+      <div class="grid-item">8</div>
+      <div class="grid-item">9</div>
+      <div class="grid-item">1</div>
+      <div class="grid-item">2</div>
+      <div class="grid-item">3</div>
+      <div class="grid-item">4</div>
+      <div class="grid-item">5</div>
+      <div class="grid-item">6</div>
+      <div class="grid-item">7</div>
+      <div class="grid-item">8</div>
+      <div class="grid-item">9</div>
+      <div class="grid-item">1</div>
+      <div class="grid-item">4</div>
+      <div class="grid-item">5</div>
+      <div class="grid-item">6</div>
+      <div class="grid-item">7</div>
+      <div class="grid-item">8</div>
+      <div class="grid-item">9</div>
+    </div>
   </div>
 );
+
+/*
+<div style={{'display':'flex','justify-content':'center','align-items':'center','height':'100vh'}}>
+</div>
+<div>
+  Money: <span id='money'></span>
+  <a class='btn btn-primary' onClick={makeMoney}>Make Money</a>
+</div>
+*/
 
 window.setInterval(function() {
   try {
