ade85a7f5ae063dab90693468cef0eabe7172a33
John Stoermer <jstoerm@purdue.edu>
2020-01-25 22:02:57 -0500

    added some crazy pic and stuff

--- src/components/Farm/index.js
+++ src/components/Farm/index.js
@@ -18,8 +18,24 @@ function makeMoney() {
   updateLabel('money', money);
 }
 
-function displayCrop(plotIndex) {
+function harvestCrop(plotIndex) {
+  if (plots[plotIndex] !=0) {
+    plots[plotIndex] = 0;
+    resources[plots[plotIndex]]++;
+  }
+}
 
+function plantCrops(cropType, num) {
+  var cnt = 0;
+  for (const plot in plots) {
+    if (cnt == num) {
+      break;
+    }
+    if (plots[plot] != 0) {
+      plots[plot] = cropType;
+      cnt++;
+    }
+  }
 }
 
 function updateResources() {
@@ -37,26 +53,104 @@ function updateResources() {
 
 const Farm = () => (
   <div>
-    {/*LEFT BAR*/}
-    <div class='row'>
-      <div class='col-md-9' style={{'height': '100vh', 'background-color': 'red'}}>
-        <div style={{'display':'flex','justify-content':'center','align-items':'center','height':'100%'}}>
-          <div class='grid-container'>
-            <div class='grid-item' id='plot-0' onClick={displayCrop(0)}>
-              <img src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
-            </div>
+    {/*background*/}
+    <div class='backpic' style={{'width': '100vw', 'height': '100vh', 'position': 'absolute'}}>
+      <div style={{'display':'flex','justify-content':'center','align-items':'center','height':'100%'}}>
+        <div class='grid-container'>
+          <div class='grid-item' onClick={harvestCrop(0)}>
+            <img id='plot-0' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(1)}>
+            <img id='plot-1' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(2)}>
+            <img id='plot-2' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(3)}>
+            <img id='plot-3' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(4)}>
+            <img id='plot-4' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(5)}>
+            <img id='plot-5' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(6)}>
+            <img id='plot-6' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(7)}>
+            <img id='plot-7' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(8)}>
+            <img id='plot-8' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(9)}>
+            <img id='plot-9' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(10)}>
+            <img id='plot-10' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(11)}>
+            <img id='plot-11' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(12)}>
+            <img id='plot-12' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(13)}>
+            <img id='plot-13' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(14)}>
+            <img id='plot-14' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(15)}>
+            <img id='plot-15' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(16)}>
+            <img id='plot-16' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(17)}>
+            <img id='plot-17' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(18)}>
+            <img id='plot-18' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(19)}>
+            <img id='plot-19' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(20)}>
+            <img id='plot-20' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(21)}>
+            <img id='plot-21' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(22)}>
+            <img id='plot-22' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(23)}>
+            <img id='plot-23' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
+          </div>
+          <div class='grid-item' onClick={harvestCrop(24)}>
+            <img id='plot-24' src={'http://cssgridgarden.com/images/dirt.svg'} style={{'height': '100%', 'width': '100%'}}></img>
           </div>
         </div>
       </div>
-      {/*RIGHT BAR*/}
-      <div class='col-md-3' style={{'height': '100vh', 'background-color': 'green'}}>
-        <div class='row round2'>
-          <p>Money: <span id='money'>0</span></p>
-          <p>Engines: <span id='engines'>0</span></p>
-          <p>Tires: <span id='tires'>0</span></p>
-          <p>Steering Mechanisms: <span id='steering'>0</span></p>
-          <p>Transmission Gears: <span id='gears'>0</span></p>
-          <p>Hydraulic Systems: <span id='systems'>0</span></p>
+    </div>
+    {/*RIGHT BAR*/}
+    <div class='row'>
+      <div class='col-md-9'></div>
+      <div class='col-md-3' style={{'background-color': 'green'}}>
+        <div class='row'>
+          <div class='badge'>Money: <span id='money'>0</span></div>
+          <br></br>
+          <div class='badge'>Engines: <span id='engines'>0</span></div>
+          <br></br>
+          <div class='badge'>Tires: <span id='tires'>0</span></div>
+          <br></br>
+          <div class='badge'>Steering Mechanisms: <span id='steering'>0</span></div>
+          <br></br>
+          <div class='badge'>Transmission Gears: <span id='gears'>0</span></div>
+          <br></br>
+          <div class='badge'>Hydraulic Systems: <span id='systems'>0</span></div>
         </div>
         <div class='row'>
           <div class='col-md-5'>
@@ -69,19 +163,6 @@ const Farm = () => (
         <div class='row'>
           <div class='col-md-5'>
             <button class='btn btn-primary'>Shop</button>
-          </div>
-          <div class='col-md-5'>
-          <Popup trigger={<button class='btn btn-primary'>Battle</button>} position="top left">
-          {close => (
-            <div>
-              Content here
-              <a className="close" onClick={close}>
-                &times;
-              </a>
-            </div>
-          )}
-          </Popup>
-
           </div>
         </div>
       </div>
